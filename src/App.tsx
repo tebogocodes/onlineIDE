949ffddcfe898375ef74f310312ecd877d80b785
tebogocodes <151438641+tebogocodes@users.noreply.github.com>
2024-02-18 00:37:32 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import { ThemeProvider } from "@/components/theme-provider";
+mport { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "sonner";
-import { useEffect } from "react";
-import { useGetUserDetailsQuery } from "./redux/slices/api";
-import { useDispatch } from "react-redux";
-import { updateCurrentUser, updateIsLoggedIn } from "./redux/slices/appSlice";
-import AllRoutes from "./AllRoutes";
+
+
 
 function App() {
-  const { data, error } = useGetUserDetailsQuery();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    if (data) {
-      dispatch(updateCurrentUser(data));
-      dispatch(updateIsLoggedIn(true));
-    } else if (error) {
-      dispatch(updateCurrentUser({}));
-      dispatch(updateIsLoggedIn(false));
-    }
-  }, [data, error]);
+  
 
   return (
     <>
